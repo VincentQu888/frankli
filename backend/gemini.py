f18afc9b1cb0258290d3215f35bda882e129afec
Vincent Qu <vincentqu888@gmail.com>
2025-06-22 10:02:44 -0400

    Delete backend directory

--- backend/gemini.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyC_SZPN-cpkm4e1KoIRlVcRLhgbrQv7s18")
-
-model = genai.GenerativeModel("gemini-2.5-flash")
-response = model.generate_content("Explain how AI works in a few words")
-
-print(response.text)
